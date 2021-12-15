1bd8b0038b090c6712c3a6b5ba5bbd8379e9771e
Veronica Lemos <veulemos@gmail.com>
2021-12-15 15:33:28 -0300

    adicionando o arquivo Locarweb

--- src/App.js
+++ src/App.js
@@ -1,24 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+
+import Home from './components/pages/Home'
+import Company from './components/pages/Company'
+import Contact from './components/pages/Contact'
+import Client from './components/pages/Client'
+import Reservas from './components/pages/Reservas'
+import CustomerRegistration from './components/pages/CustomerRegistration'
+import NewReserva from './components/pages/NewReserva'
+import Funcionario from './components/pages/Funcionario'
+import  RentCar from './components/pages/RentCar'
+
+import Container from './components/layout/Container'
+import Navbar from './components/layout/Navbar'
+import Footer from './components/layout/Footer'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Navbar />
+      <Container customClass="min-height">
+        <Routes>
+          <Route path="/" element={<Home/>} />
+          <Route  path="/client" element={<Client/>}/>
+          <Route  path="/reservas" element={<Reservas/>}/>
+          <Route path="/company" element={<Company/>}/>
+          <Route path="/contact" element={<Contact/>}/>
+          <Route path="/customerregistration" element={<CustomerRegistration/>}/>
+          <Route  path="/newreserva" element={<NewReserva/>}/>
+          <Route  path="/funcionario" element={<Funcionario/>}/>
+          <Route  path="/rentcar" element={<RentCar/>}/>
+       </Routes>
+      </Container>
+      <Footer />
+    </Router>
+   
   );
 }
 
